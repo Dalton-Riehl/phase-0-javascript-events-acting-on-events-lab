c5ce056ad8ebdce0f808d9c406f5c92636ffb75d
Dalton Riehl <dtonriehl@gmail.com>
2024-02-27 17:01:59 -0600

    Rock Dodger Lab Revisit Complete: Achievement Get!

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
     if (left > 0) {
-        dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left - 3}px`;
     }
 }
+
 function moveDodgerRight() {
     const rightNumbers = dodger.style.left.replace("px", "");
     const right = parseInt(rightNumbers, 10);
-    if (360 > right > 0) {
-        dodger.style.left = `${right + 1}px`;
+    if (right < 360) {
+    dodger.style.left = `${right + 3}px`;
     }
 }
-document,addEventListener("keydown", function (e) {
+
+document.addEventListener('keydown', function(e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
     }
-    else if (e.key === "ArrowRight") {
+})
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
         moveDodgerRight();
     }
-});
+})
